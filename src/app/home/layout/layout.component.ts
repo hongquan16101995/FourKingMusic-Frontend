3c28a065cf7e31cbe556b5cdedd3e8f1aa0c94ba
herokillv1 <nguyentrunghieu.hha101196@gmail.com>
2020-11-04 15:17:17 +0700

    thêm link API service song

--- src/app/home/layout/layout.component.ts
+++ src/app/home/layout/layout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
--- src/app/service/song.service.ts
+++ src/app/service/song.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Song} from '../model/Song';
+
+const API_URL = `${environment.apiUrl}`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class SongService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllSongs(): Observable<Song[]> {
+    return this.http.get<Song[]>(API_URL + '/song');
+  }
+
+  getById(id: number): Observable<Song> {
+    return this.http.get<Song>(API_URL + '/song' + id);
+  }
+
+  createSong(song: Song): Observable<Song> {
+    return this.http.post<Song>(API_URL + '/song', song);
+  }
+
+  updateSong(song: Song): Observable<Song> {
+    return this.http.put<Song>(API_URL + '/song' + song.id, song);
+  }
+
+  deleteOneBook(id: number): Observable<any> {
+    return this.http.delete(API_URL + '/song' + id);
+  }
 }
