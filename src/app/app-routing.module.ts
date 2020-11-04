a8251939a814ffc1246f26948038192cf47c5096
admin <daominhhiep97@gmail.com>
2020-11-04 15:12:48 +0700

    login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LayoutComponent} from './home/layout/layout.component';
+import {LoginComponent} from './core/login/login.component';
+import {RegisterComponent} from './core/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent
+  },
+  {
+    path: 'login', component: LoginComponent,
+  },
+  {
+    path: 'register', component: RegisterComponent,
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
