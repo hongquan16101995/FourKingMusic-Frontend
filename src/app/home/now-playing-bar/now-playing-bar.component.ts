6427059cd2e31e532f134dc4493e574d6d6fc7ea
admin <daominhhiep97@gmail.com>
2020-11-04 17:10:49 +0700

    now playing bar

--- src/app/home/now-playing-bar/now-playing-bar.component.ts
+++ src/app/home/now-playing-bar/now-playing-bar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-now-playing-bar',
   templateUrl: './now-playing-bar.component.html',
-  styleUrls: ['./now-playing-bar.component.css']
+  styleUrls: ['./now-playing-bar.component.scss']
 })
 export class NowPlayingBarComponent implements OnInit {
 
