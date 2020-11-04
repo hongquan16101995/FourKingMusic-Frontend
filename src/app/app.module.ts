5956c1096a2ee2ba7f3c03d30b6f79f34713e3bd
admin <daominhhiep97@gmail.com>
2020-11-04 08:19:56 +0700

    init component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './core/login/login.component';
+import { RegisterComponent } from './core/register/register.component';
+import { LayoutComponent } from './home/layout/layout.component';
+import { NavbarMenuComponent } from './home/navbar-menu/navbar-menu.component';
+import { NowPlayingBarComponent } from './home/now-playing-bar/now-playing-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LayoutComponent,
+    NavbarMenuComponent,
+    NowPlayingBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/now-playing-bar/now-playing-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-now-playing-bar',
+  templateUrl: './now-playing-bar.component.html',
+  styleUrls: ['./now-playing-bar.component.css']
+})
+export class NowPlayingBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
