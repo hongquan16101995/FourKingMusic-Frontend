25f5f20f1b47d4470226c03c8e8f7d822fd0330d
herokillv1 <nguyentrunghieu.hha101196@gmail.com>
2020-11-05 11:08:23 +0700

    done creat-song

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './core/login/login.component';
-import { RegisterComponent } from './core/register/register.component';
-import { NavbarMenuComponent } from './home/navbar-menu/navbar-menu.component';
-import { NowPlayingBarComponent } from './home/now-playing-bar/now-playing-bar.component';
-import { LayoutComponent } from './home/layout/layout.component';
-import { ListSongComponent } from './song/list-song/list-song.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './core/login/login.component';
+import {RegisterComponent} from './core/register/register.component';
+import {NavbarMenuComponent} from './home/navbar-menu/navbar-menu.component';
+import {NowPlayingBarComponent} from './home/now-playing-bar/now-playing-bar.component';
+import {LayoutComponent} from './home/layout/layout.component';
+import {ListSongComponent} from './song/list-song/list-song.component';
 import {HttpClientModule} from '@angular/common/http';
-import { CreatSongComponent } from './song/creat-song/creat-song.component';
+import {CreatSongComponent} from './song/creat-song/creat-song.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
@@ -26,9 +30,13 @@ import { CreatSongComponent } from './song/creat-song/creat-song.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
