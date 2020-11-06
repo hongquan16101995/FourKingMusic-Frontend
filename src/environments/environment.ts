a21d155ec89d2232cbcd9a7c63dee4e8634d3837
herokillv1 <nguyentrunghieu.hha101196@gmail.com>
2020-11-06 22:53:35 +0700

    chuẩn bị sửa giao diện

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8080'
-  apiUrl: 'http://10.30.0.31:8080'
+  apiUrl: 'http://localhost:8080'
+  // apiUrl: 'http://10.30.0.31:8080'
 };
 
 /*
