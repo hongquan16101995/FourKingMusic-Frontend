41c8e9bcabf4e1999568e094fde467b25751648b
herokillv1 <nguyentrunghieu.hha101196@gmail.com>
2020-11-10 10:50:03 +0700

    chỉnh app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,14 @@ import {RegisterComponent} from './core/register/register.component';
 import {NavbarMenuComponent} from './home/navbar-menu/navbar-menu.component';
 import {LayoutComponent} from './home/layout/layout.component';
 import {HttpClientModule} from '@angular/common/http';
-import {CreatSongComponent} from './user/user-song/creat-song/creat-song.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import { SingerComponent } from './home/singer/singer-list/singer.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { PosterComponent } from './core/poster/poster.component';
-import { UserMysongComponent } from './user/user-song/user-mysong/user-mysong.component';
 import { UserHomeComponent } from './user/user-home/user-home.component';
 import { UserProfileComponent } from './user/user-profile/user-profile.component';
-import {DeleteSongComponent} from './user/user-song/delete-song/delete-song.component';
-import { UserEditMysongComponent } from './user/user-song/user-edit-mysong/user-edit-mysong.component';
 import {environment} from '../environments/environment';
-import { UserPlaySongComponent } from './user/user-song/user-play-song/user-play-song.component';
 import {PasswordComponent} from './user/password/password.component';
 import {NewMostSongsComponent} from './home/song/new-most-songs/new-most-songs.component';
 import {ListPlaylistComponent} from './home/song/list-playlist/list-playlist.component';
@@ -31,6 +26,11 @@ import {PlaySongComponent} from './home/song/play-song/play-song.component';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFireStorageModule} from '@angular/fire/storage';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {CreatSongComponent} from './user/user-song/creat-song/creat-song.component';
+import {DeleteSongComponent} from './user/user-song/delete-song/delete-song.component';
+import {UserMysongComponent} from './user/user-song/user-mysong/user-mysong.component';
+import {UserEditMysongComponent} from './user/user-song/user-edit-mysong/user-edit-mysong.component';
+import {UserPlaySongComponent} from './user/user-song/user-play-song/user-play-song.component';
 
 
 
