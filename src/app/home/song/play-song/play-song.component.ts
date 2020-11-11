d7528325b689412931295147b272bb81a0de6156
admin <daominhhiep97@gmail.com>
2020-11-11 11:54:08 +0700

    phát nhạc đơn done

--- src/app/home/song/play-song/play-song.component.ts
+++ src/app/home/song/play-song/play-song.component.ts
@@ -47,37 +47,7 @@ export class PlaySongComponent implements OnInit {
             cover_art_url: data.avatarUrl
           },
         ],
-        callbacks: {
-          // tslint:disable-next-line:typedef
-          play() {
-            document.getElementById('album-art').style.visibility = 'hidden';
-            document.getElementById('large-visualization').style.visibility = 'visible';
-          },
-
-          // tslint:disable-next-line:typedef
-          pause() {
-            document.getElementById('album-art').style.visibility = 'visible';
-            document.getElementById('large-visualization').style.visibility = 'hidden';
-          }
-        },
-        waveforms: {
-          sample_rate: 50
-        }
       });
     });
-
-    /*
-    When the bandcamp link is pressed, stop all propagation so AmplitudeJS doesn't
-    play the song.
-    */
-    const bandcampLinks = document.getElementsByClassName('bandcamp-link');
-
-    // tslint:disable-next-line:prefer-for-of no-shadowed-variable
-    for (let i = 0; i < bandcampLinks.length; i++) {
-      // tslint:disable-next-line:only-arrow-functions typedef
-      bandcampLinks[i].addEventListener('click', function(e) {
-        e.stopPropagation();
-      });
-    }
   }
 }
--- src/app/user/user-song/user-play-song/user-play-song.component.ts
+++ src/app/user/user-song/user-play-song/user-play-song.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Song} from '../../../model/Song';
 import {SongService} from '../../../service/song.service';
 import {ActivatedRoute} from '@angular/router';
+import {Singers} from '../../../model/Singers';
+declare var Amplitude: any;
 
 @Component({
   selector: 'app-user-play-song',
@@ -10,10 +12,11 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class UserPlaySongComponent implements OnInit {
 
-  song: Song;
   id: number;
+  song: Song;
   avaUrl: string;
   name: string;
+  singers: Singers[];
   fileURL: string;
   user: string;
 
@@ -23,16 +26,28 @@ export class UserPlaySongComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = Number(this.router.snapshot.paramMap.get('id'));
+    console.log(this.id);
     this.songService.getSongById(this.id).subscribe(data => {
       this.song = {
         id: data.id,
         user: data.user,
+        singers: data.singers,
+        dateCreated: data.dateCreated,
       };
       this.avaUrl = data.avatarUrl;
       this.name = data.name;
+      this.singers = data.singers;
       this.fileURL = data.fileUrl;
       this.user = data.user.name;
+
+      Amplitude.init({
+        songs: [
+          {
+            url: data.fileUrl,
+            cover_art_url: data.avatarUrl
+          },
+        ],
+      });
     });
   }
-
 }
